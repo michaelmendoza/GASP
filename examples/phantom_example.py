ebda5059a8c73b0695540022a7fc572a51b6e55c
Michael Mendoza <m.mendoza711@gmail.com>
2019-04-05 16:28:09 -0600

    Added periodic forcing function

--- examples/phantom_example.py
+++ examples/phantom_example.py
@@ -6,9 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mr_utils import view
+from mr_utils.recon.ssfp import gs_recon
 from tqdm import trange
+from skimage.filters import threshold_li
 
-from gasp import gasp, triangle
+from gasp import gasp, triangle, triangle_periodic 
 
 if __name__ == '__main__':
   
@@ -21,6 +23,16 @@ if __name__ == '__main__':
 
   data = np.concatenate((data0[..., None], data1[..., None], data2[..., None]), axis=-1)
   data = np.mean(data, axis=3) # [Height, Width, Coil, PCs, TRs]
+
+  # Create a mask of the phanton
+  band_free = gs_recon(data[:, :, 0, ::4, 0], pc_axis=-1)
+  thresh = threshold_li(np.abs(band_free))
+  mask = np.abs(band_free) > thresh
+
+  # Apply mask to data
+  mask0 = np.tile(mask, (data.shape[2:] + (1, 1,))).transpose((3, 4, 0, 1, 2))
+  data = data*mask0
+
   print(data.shape[:-2])
   data = np.reshape(data, data.shape[:-2] + (-1,)) # [Height, Width, Coil, PCs x TRs]
   data = np.moveaxis(data, 2, 0) # [Coil, Height, Width, PCs x TRs]
@@ -29,8 +41,12 @@ if __name__ == '__main__':
 
   # view(data, fft_axes=(-2, -1), montage_axis=0, movie_axis=1, movie_interval=200)
 
-  D = triangle(np.linspace(-100, 100, height), bw=10)
-  D = np.roll(D, int(height/9))
+  #D = triangle(np.linspace(-100, 100, height), bw=10)
+  #D = triangle_periodic(height, 32, 0, 16)
+  #D = triangle_periodic(height, 76, 0, 38)
+  D = triangle_periodic(height, 76, 18, 38)
+  D *= mask[:, int(width/2)]
+  #D = np.roll(D, int(height/9))
   plt.plot(D)
   plt.show()
 
--- gasp/responses.py
+++ gasp/responses.py
@@ -1,12 +1,13 @@
-
+import math
 import numpy as np
+from scipy.signal import triang
 
 def triangle(x0, bw):
     '''Spatial forcing function.
 
     Parameters
     ----------
-    x0 : float
+    x0 : array_like
         Location (in px).
     bw : float
         Bandwidth of forcing function in Hz, e.g., 1/TR.
@@ -27,3 +28,22 @@ def triangle(x0, bw):
             out[jj] = 1 - np.abs(xx)
     out[np.abs(out) > 0] -= np.min(out)
     return out/np.max(np.abs(out))
+
+def triangle_periodic(img_width, period, offset, bw):
+
+    assert period >= bw
+
+    window = triang(bw)
+    window = np.concatenate((window, np.zeros(period - bw)))
+    num_of_windows = math.ceil(img_width / period)
+    response = np.tile(window, (num_of_windows))
+    response = response[:img_width]
+    response = np.roll(response, offset)
+    return response
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    plt.plot(triangle_periodic(128, 32, 8, 16))
+    #plt.plot(triangle_periodic(128, 32, 8, 32))
+    plt.show()
