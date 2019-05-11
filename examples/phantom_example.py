5d7f1f001d33befb4abf96727c2f83c740ca9d2a
mckib2 <nicholas.bgp@gmail.com>
2019-05-11 15:37:33 -0600

    Water phantom with long TR now working as expected.

--- examples/phantom_example.py
+++ examples/phantom_example.py
@@ -1,6 +1,6 @@
 
+import os
 import sys
-sys.path.insert(0, './')
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,65 +11,83 @@ from tqdm import trange
 from skimage.filters import threshold_li
 from time import time
 
-from gasp import gasp, triangle, triangle_periodic 
+from gasp import gasp, triangle, triangle_periodic
+
+if os.name == 'nt':
+    sys.path.insert(0, './')
 
 if __name__ == '__main__':
-  
-  dataset = 1
-  t0 = time()
-  data0 = np.load('data/20190401_GASP_PHANTOM/set%d_tr6_te3.npy' % dataset)
-  data1 = np.load('data/20190401_GASP_PHANTOM/set%d_tr12_te6.npy' % dataset)
-  data2 = np.load('data/20190401_GASP_PHANTOM/set%d_tr24_te12.npy' % dataset)
-  print(time() - t0)
 
-  print(data0.shape) # [Height, Width, Coil, Avg, PCs]
+    # path = 'data/20190401_GASP_PHANTOM'
+    # dataset = 1
+    # TEs = [3, 6, 12]
 
-  data = np.concatenate((data0[..., None], data1[..., None], data2[..., None]), axis=-1)
-  data = np.mean(data, axis=3) # [Height, Width, Coil, PCs, TRs]
+    path = 'data/20190507_GASP_LONG_TR_WATER'
+    dataset = 1
+    TEs = [12, 24, 48]
 
-  # Create a mask of the phanton
-  band_free = gs_recon(data[:, :, 0, ::4, 0], pc_axis=-1)
-  thresh = threshold_li(np.abs(band_free))
-  mask = np.abs(band_free) > thresh
+    data = []
+    t0 = time()
+    for ii, te in enumerate(TEs):
+        data.append(np.load('%s/set%d_tr%d_te%d.npy' % (
+            path, dataset, te*2, te))[..., None])
+    data = np.concatenate(data, axis=-1)
+    print(time() - t0)
+    print(data[0].shape) # [Height, Width, Coil, Avg, PCs]
 
-  # Apply mask to data
-  mask0 = np.tile(mask, (data.shape[2:] + (1, 1,))).transpose((3, 4, 0, 1, 2))
-  data = data*mask0
+    # Collapse the averages dimension
+    data = np.mean(data, axis=3) # [Height, Width, Coil, PCs, TRs]
 
-  print(data.shape[:-2])
-  data = np.reshape(data, data.shape[:-2] + (-1,)) # [Height, Width, Coil, PCs x TRs]
-  data = np.moveaxis(data, 2, 0) # [Coil, Height, Width, PCs x TRs]
-  data = data.transpose((0, 3, 2, 1)) # [Coil,  PCs x TRs, Width, Height]
-  
-  ncoils, npcs, height, width = data.shape[:]
-  C_dim = (32, width) # Calibration box - (# Number of lines of calibration, Pixels on signal)
+    # Create a mask of the phanton
+    band_free = gs_recon(data[:, :, 0, ::4, 0], pc_axis=-1)
+    thresh = threshold_li(np.abs(band_free))
+    mask = np.abs(band_free) > thresh
 
-  # view(data, fft_axes=(-2, -1), montage_axis=0, movie_axis=1, movie_interval=200)
+    # Apply mask to data
+    mask0 = np.tile(
+        mask,
+        (data.shape[2:] + (1, 1,))).transpose((3, 4, 0, 1, 2))
+    data = data*mask0
 
-  #D = triangle(np.linspace(-100, 100, height), bw=10)
-  #D = triangle_periodic(height, 32, 0, 16)
-  #D = triangle_periodic(height, 76, 0, 38)
-  D = triangle_periodic(width, 76, 18, 38)
-  D *= mask.T[int(height/2), :]
-  #D = np.roll(D, int(height/9))
-  plt.plot(D)
-  plt.show()
+    print(data.shape[:-2])
+    data = np.reshape(data, data.shape[:-2] + (-1,))
+    # [Height, Width, Coil, PCs x TRs]
+    data = np.moveaxis(data, 2, 0)
+    # [Coil, Height, Width, PCs x TRs]
+    data = data.transpose((0, 3, 2, 1))
+    # [Coil,  PCs x TRs, Width,   Height]
 
-  Ic = np.zeros((ncoils, height, width), dtype='complex')
-  for cc in trange(ncoils, leave=False):
-    Ic[cc, ...] = gasp(data[cc, ...], D, C_dim, pc_dim=0)
-  Ic = np.sqrt(np.sum(np.abs(Ic)**2, axis=0))
-  # Ic = np.abs(Ic[2, ...])
+    # Get new dimensions
+    ncoils, npcs, height, width = data.shape[:]
 
-  plt.subplot(1, 3, 1)
-  plt.imshow(np.sqrt(np.sum(abs(data[:, 0, ...])**2, axis=0)))
+    # Calibration box - (# Number of lines of calibration, Pixels on
+    # signal)
+    C_dim = (32, width)
 
-  plt.subplot(1, 3, 2)
-  plt.imshow(Ic)
+    #D = triangle(np.linspace(-100, 100, height), bw=10)
+    #D = triangle_periodic(height, 32, 0, 16)
+    #D = triangle_periodic(height, 76, 0, 38)
+    D = triangle_periodic(width, 76, 18, 38)
+    D *= mask.T[int(height/2), :]
+    #D = np.roll(D, int(height/9))
+    plt.plot(D)
+    plt.show()
 
-  plt.subplot(1, 3, 3)
-  plt.plot(np.abs(Ic[int(height/2), :]), label='Simulated Profile')
-  plt.plot(D, '--', label='Desired Profile')
-  plt.legend()
-  plt.title('Center horizontal slice\'s spatial response profile')
-  plt.show()
+    Ic = np.zeros((ncoils, height, width), dtype='complex')
+    for cc in trange(ncoils, leave=False):
+        Ic[cc, ...] = gasp(data[cc, ...], D, C_dim, pc_dim=0)
+    Ic = np.sqrt(np.sum(np.abs(Ic)**2, axis=0))
+    # Ic = np.abs(Ic[2, ...])
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(np.sqrt(np.sum(abs(data[:, 0, ...])**2, axis=0)))
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(Ic)
+
+    plt.subplot(1, 3, 3)
+    plt.plot(np.abs(Ic[int(height/2), :]), label='Simulated Profile')
+    plt.plot(D, '--', label='Desired Profile')
+    plt.legend()
+    plt.title('Center horizontal slice\'s spatial response profile')
+    plt.show()
