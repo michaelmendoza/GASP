a63a3f098c30b955d6beeb55f90d4c79abd403f7
Nicholas McKibben <nicholas.bgp@gmail.com>
2024-03-26 09:34:19 -0700

    ENH: add phase cycle parameter optimization notebook 23

--- gasp/ssfp.py
+++ gasp/ssfp.py
@@ -12,7 +12,7 @@ def ssfp(T1, T2, TR, TE, alpha, dphi=(0,), field_map=0, M0=1, f0=0, phi=0, useSq
         M.append(_ssfp(T1, T2, TR, TE, alpha, pc, field_map, M0, f0, phi)[..., None])
     M = np.concatenate(M, axis=-1)
     
-    # Squeeze out dim of length 1, otherwise shape is [width, height, dpi]
+    # Squeeze out dim of length 1, otherwise shape is [width, height, dphi]
     M = np.squeeze(M)
     if not useSqueeze and len(dphi) == 1:
         M = M[..., None]
